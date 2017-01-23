c0834058dd3cad552c99a54126a1431e80b84359
Anton Kechashin <atk@sitecore.net>
2017-01-23 13:12:04 +0200

    Initial version

---
+++ src/Sitecore.Support.139757/Pipelines/Loader/RegisterjQuery.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.Support.Pipelines.Loader
+{
+    using Sitecore.Pipelines;
+    using System.Web.UI;
+
+    public class RegisterjQuery
+    {
+        public void Process(PipelineArgs args)
+        {
+            ScriptResourceDefinition definition = new ScriptResourceDefinition
+            {
+                #region Original code
+                //Path = "~/sitecore/controls/shell/Controls/Lib/jQuery/jquery-1.10.2.min.js", 
+                #endregion
+                #region Modified code
+                Path = "~/sitecore/shell/Controls/Lib/jQuery/jquery-1.10.2.min.js", 
+                #endregion
+                CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.10.2.min.js",
+                CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.10.2.js"
+            };
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", definition);
+        }
+    }
+}
---
+++ src/Sitecore.Support.139757/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.139757")]
+[assembly: AssemblyProduct("Sitecore.Support.139757")]
+[assembly: ComVisible(false)]
